70048bd91b4fd4db4272d01cda2678a498ea2a2a
jeffreyferrert <jeffrey.ferrert@gmail.com>
2023-06-09 17:29:08 -0400

    index/show pages

--- ninety-nine-cats/db/migrate/20230609191024_create_cat.rb
+++ ninety-nine-cats/db/migrate/20230609191024_create_cat.rb
@@ -7,7 +7,7 @@ class CreateCat < ActiveRecord::Migration[7.0]
 
       t.date :birth_date, null:false
       t.string :color, null:false
-      t.string :name, null:false
+      t.string :name, null:false, index: true
       t.string :sex, limit: 1, null:false
       t.text :description
 
--- ninety-nine-cats/db/schema.rb
+++ ninety-nine-cats/db/schema.rb
@@ -22,6 +22,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_06_09_191024) do
     t.text "description"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["name"], name: "index_cats_on_name"
   end
 
 end
